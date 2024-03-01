2632b1eb41774ec13503140a82cd6fe613a6974d
Niko <radosz2003@gazeta.pl>
2024-03-01 16:52:27 +0100

    done :3

--- kms.c
+++ kms.c
@@ -5,6 +5,8 @@ int main(){
 
     //Initialise I/O
     stdio_init_all(); 
+		
+		gpio_init(25);
 
     gpio_init(13); // resolution
     gpio_init(12); // resolution
@@ -19,53 +21,60 @@ int main(){
     gpio_set_dir(11, GPIO_OUT); // res
     gpio_set_dir(10, GPIO_OUT); // dir
     gpio_set_dir(0, GPIO_OUT);	// step 
+		gpio_set_dir(25, GPIO_OUT);
 
+		gpio_put(25,1);
 
-
-	char UserInput;
-
-
-		gpio_put(10, 1); // dir
+		int res = 0;
+		int dir = 0;
 
     while(1){
+			printf(" Res = %d, Dir = %d \n", res, dir);
+			printf("Set Res & Dir: \n");
+			scanf("%d %d", &res, &dir);
+			printf(" Res = %d, Dir = %d \n", res, dir);
 			
-			printf("Set Res: \n");
+			if (dir==0) 
+				{gpio_put(10,0);}
+			else if (dir==1)
+				{gpio_put(10,1);}
+			else 
+			{gpio_put(10,0);
+			 printf("WRONG DIR\n");}
 
-			UserInput = getchar();
-
-     	switch (UserInput) {
+     	switch (res) {
   
-			case '0': // full
+			case 0: // full
 			{gpio_put(11 ,0);
      	gpio_put(12 ,0);
      	gpio_put(13 ,0);}	
     	break;
   
-			case '1': // 1/2
+			case 1: // 1/2
     	{gpio_put(11 ,1);
      	gpio_put(12 ,0);
      	gpio_put(13 ,0);}	
     	break;
 	
-			case '2': // 1/4
+			case 2: // 1/4
     	{gpio_put(11 ,0);
      	gpio_put(12 ,1);
      	gpio_put(13 ,0);}	
     	break;
 	
-			case '3': // 1/8
+			case 3: // 1/8
     	{gpio_put(11 ,1);
      	gpio_put(12 ,1);
      	gpio_put(13 ,0);}	
     	break;
 	
-			case '4': // 1/16
+			case 4: // 1/16
    		{gpio_put(11 ,0);
    	  gpio_put(12 ,0);
    	  gpio_put(13 ,1);}	
    		break;
 
-			case '5': // 1/32
+			case 5: // 1/32
    	  {gpio_put(11 ,1);
    	  gpio_put(12 ,0);
    	  gpio_put(13 ,1);}	
@@ -74,7 +83,8 @@ int main(){
   		default:
   	  {gpio_put(11 ,1);
     	 gpio_put(12 ,0);
-     	gpio_put(13 ,1);}	
+     	 gpio_put(13 ,1);
+			 printf("WRONG Res\n");}	
 		}
    gpio_put(0, 1); // Set pin 0 to high
         printf("Step!\n");
@@ -82,7 +92,8 @@ int main(){
         gpio_put(0, 0); // Set pin 0 to low
 				printf("stepus \n");
         sleep_ms(500); // 0.5s delay
-    
-
+				res = 5;
+				dir = 0;
+				printf("reset \n");
 		}
 }

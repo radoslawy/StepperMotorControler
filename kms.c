9c2c11e87399cff17476abfab99392215fdd4f04
Niko <radosz2003@gazeta.pl>
2024-04-21 21:22:39 +0200

    finished i hope

--- kms.c
+++ kms.c
@@ -1,7 +1,64 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main(){
+
+void setRes (){
+		printf("set res (0-5): ");
+		int res = getchar()-48;
+		printf("\n res: %d\n",res);
+		switch (res) {
+  
+			case 0: // full
+			{gpio_put(11 ,0);
+     	gpio_put(12 ,0);
+     	gpio_put(13 ,0);
+			printf("res full \n");}
+    	break;
+  
+			case 1: // 1/2
+    	{gpio_put(11 ,1);
+     	gpio_put(12 ,0);
+     	gpio_put(13 ,0);	
+			printf("res 1/2 \n");}
+    	break;
+	
+			case 2: // 1/4
+    	{gpio_put(11 ,0);
+     	gpio_put(12 ,1);
+     	gpio_put(13 ,0);	
+			printf("res 1/4 \n");}
+    	break;
+	
+			case 3: // 1/8
+    	{gpio_put(11 ,1);
+     	gpio_put(12 ,1);
+     	gpio_put(13 ,0);
+			printf("res 1/8 \n");}
+    	break;
+	
+			case 4: // 1/16
+   		{gpio_put(11 ,0);
+   	  gpio_put(12 ,0);
+   	  gpio_put(13 ,1);
+			printf("res 1/16 \n");}
+   		break;
+
+			case 5: // 1/32
+   	  {gpio_put(11 ,1);
+   	  gpio_put(12 ,0);
+   	  gpio_put(13 ,1);	
+			printf("res 1/32 \n");}
+   	 	break;
+
+  		default:
+  	  {gpio_put(11 ,1);
+    	 gpio_put(12 ,0);
+     	gpio_put(13 ,1);
+			printf("RES def \n");}	
+		}
+}
+
+void setup(){
 
     //Initialise I/O
     stdio_init_all(); 
@@ -25,78 +82,45 @@ int main(){
 
 		gpio_put(1,1); //CONST ON
 		gpio_put(10, 1); // dir
+}
 
-	char UserInput;
+void setDir(){
+
+	printf("Set Dir (1 or 0): ");
+	int dir = getchar()-48;
+	printf("dir %d\n",dir);
+	if (dir != 0 && dir != 1){
+		dir = 0;
+		printf("wrong dir\n");
+	}
+
+	gpio_put(10,dir);
+}
+
+int setRep(){
 	int rep=1;
-    while(1){
-			printf("Rep: ");
-		UserInput = getchar();
+	printf("Rep: ");
+	scanf("%i",&rep);
+	printf("%d\n",rep);
+	return rep;
+}
 
-			printf("Set Dir: \n");
-		rep = (int)getchar();
+int main(){
 
-		if (UserInput == '0') {gpio_put(10,0);
-		printf("dir 0 \n");}
-		else {gpio_put(10,1);
-		printf("dir 1 \n");}
+	setup(); // initialize pins
 
+	getchar(); // wait for first input
 
-			printf("Set Res: \n");
+	int rep=1;
 
-			UserInput = getchar();
+  while(1){
 
-     	switch (UserInput) {
-  
-			case '0': // full
-			{gpio_put(11 ,0);
-     	gpio_put(12 ,0);
-     	gpio_put(13 ,0);
-			printf("res full \n");}
-    	break;
-  
-			case '1': // 1/2
-    	{gpio_put(11 ,1);
-     	gpio_put(12 ,0);
-     	gpio_put(13 ,0);	
-			printf("res 1/2 \n");}
-    	break;
-	
-			case '2': // 1/4
-    	{gpio_put(11 ,0);
-     	gpio_put(12 ,1);
-     	gpio_put(13 ,0);	
-			printf("res 1/4 \n");}
-    	break;
-	
-			case '3': // 1/8
-    	{gpio_put(11 ,1);
-     	gpio_put(12 ,1);
-     	gpio_put(13 ,0);
-			printf("res 1/8 \n");}
-    	break;
-	
-			case '4': // 1/16
-   		{gpio_put(11 ,0);
-   	  gpio_put(12 ,0);
-   	  gpio_put(13 ,1);
-			printf("res 1/16 \n");}
-   		break;
+		rep=setRep();
+		setRes();
+		setDir();
 
-			case '5': // 1/32
-   	  {gpio_put(11 ,1);
-   	  gpio_put(12 ,0);
-   	  gpio_put(13 ,1);	
-			printf("res 1/32 \n");}
-   	 	break;
-
-  		default:
-  	  {gpio_put(11 ,1);
-    	 gpio_put(12 ,0);
-     	gpio_put(13 ,1);
-			printf("RES def \n");}	
-		}
-
-		for(int i=0;i<=rep;i++)
+     
+		for(int i=1;i<=rep;i++)
 		{
    			gpio_put(0, 1); // Set pin 0 to high
         printf("Step!\n");
@@ -104,6 +128,8 @@ int main(){
         gpio_put(0, 0); // Set pin 0 to low
 				printf("stepus \n");
         sleep_ms(500); // 0.5s delay
+				printf("rep %d z %d\n", i, rep);
   	}
 		}
 }
+
